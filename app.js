b89bebd8743ca87f1fc47589b031cb7a0dac3bc2
Shiva Bijwe <22devendrabijwe@gmail.com>
2024-01-01 23:39:07 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files (like HTML) from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'lib')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+// Serve index.html on the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('*', (req, res) => {
+    res.status(404).send('404 Not Found');
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
